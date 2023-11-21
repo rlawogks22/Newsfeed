680217becbef4244c9c305d7f5fbf20049091efd
taeholee <lth1993@naver.com>
2023-11-21 20:23:28 +0900

    feat : 회원가입 시 정규표현식 예외처리

--- src/main/java/com/example/newsfeed/controller/UserController.java
+++ src/main/java/com/example/newsfeed/controller/UserController.java
@@ -1,16 +1,25 @@
 package com.example.newsfeed.controller;
 
 
+import com.example.newsfeed.dto.CommonResponseDto;
 import com.example.newsfeed.dto.LoginRequestDto;
 import com.example.newsfeed.dto.SignupRequestDto;
 import com.example.newsfeed.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/users")
@@ -19,8 +28,19 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public void signup(@Validated @RequestBody SignupRequestDto signupRequestDto){
+    public ResponseEntity<CommonResponseDto> signup(@Validated @RequestBody SignupRequestDto signupRequestDto,
+                                                    BindingResult bindingResult) {
+        // 정규표현식 유효성을 판단하기 위해
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if (fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+                return ResponseEntity.badRequest().body(
+                        new CommonResponseDto(fieldError.getDefaultMessage(), HttpStatus.BAD_REQUEST.value()));
+            }
+        }
         userService.signup((signupRequestDto));
+        return ResponseEntity.status(HttpStatus.OK.value()).body((
+                new CommonResponseDto("성공적으로 회원가입에 성공했습니다.", HttpStatus.OK.value())));
     }
-
 }
