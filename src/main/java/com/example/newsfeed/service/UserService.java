bd3bfd00abb649bcade2550847d3fe669ce42981
taeholee <lth1993@naver.com>
2023-11-22 13:23:01 +0900

    feat : Spring Security 를 이용한 로그인 구현 refactor : 회원가입시 비밀번호 암호화 추가/프로필 입력

--- src/main/java/com/example/newsfeed/service/UserService.java
+++ src/main/java/com/example/newsfeed/service/UserService.java
@@ -1,11 +1,13 @@
 package com.example.newsfeed.service;
 
+import com.example.newsfeed.dto.LoginRequestDto;
 import com.example.newsfeed.dto.SignupRequestDto;
 import com.example.newsfeed.entity.User;
 import com.example.newsfeed.repository.UserRepository;
 import com.fasterxml.jackson.core.SerializableString;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -15,12 +17,15 @@ import java.util.Optional;
 public class UserService {
     private final UserRepository userRepository;
     private User user;
-    private String username, email, nickname;
+    private String username, email, nickname, pwd, profile;
+    private final PasswordEncoder passwordEncoder;
     Optional<User> userOptional;
     public void signup(SignupRequestDto signupRequestDto) {
         username = signupRequestDto.getUsername();
-        email = signupRequestDto.getEmail();
+        pwd = passwordEncoder.encode(signupRequestDto.getPwd());
         nickname = signupRequestDto.getNickname();
+        email = signupRequestDto.getEmail();
+        profile = signupRequestDto.getProfile();
         userOptional = userRepository.findByUsername(username);
         if(userOptional.isPresent()){
             throw new IllegalArgumentException("중복된 회원 아이디가 있습니다.");
@@ -33,8 +38,17 @@ public class UserService {
         if(userOptional.isPresent()){
             throw new IllegalArgumentException("중복된 닉네임이 있습니다.");
         }
-        user = new User(signupRequestDto);
+        user = new User(username, pwd, nickname, email, profile);
         userRepository.save(user);
     }
 
+    public void login(LoginRequestDto loginRequestDto) {
+        username = loginRequestDto.getUsername();
+        pwd = loginRequestDto.getPwd();
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("등록된 유저가 없습니다."));
+        if(!passwordEncoder.matches(pwd, user.getPwd())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+    }
 }
